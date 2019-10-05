63d3d7beed2a443971b9351f47081a409e8ddf58
AmyliaY <471816751@qq.com>
2019-10-05 23:57:03 +0800

    优化IoC容器部分的实现

--- src/main/java/com/gupaoedu/vip/mvc/demo/service/INamedService.java
+++
@@ -1,5 +0,0 @@
-package com.gupaoedu.vip.mvc.demo.service;
-
-public interface INamedService {
-
-}
--- src/main/java/com/gupaoedu/vip/mvc/demo/service/impl/ServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.gupaoedu.vip.mvc.demo.service.impl;
-
-import com.gupaoedu.vip.mvc.demo.service.IService;
-import com.gupaoedu.vip.mvc.framework.annotation.GPService;
-
-@GPService
-public class ServiceImpl implements IService{
-
-}
